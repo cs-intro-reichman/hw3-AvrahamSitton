7faf344897ec3a2caf5c58296ab253c09444d82c
AvrahamSitton <avraham.sitton@post.runi.ac.il>
2024-11-19 21:41:57 +0200

    sdgvadf

--- Algebra.java
+++ Algebra.java
@@ -5,63 +5,100 @@
 
 public class Algebra {
 	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
-	}  
+		// Tests some of the operations
+		System.out.println("plus 5  " + plus(2, 3)); // 2 + 3
+		System.out.println("minus 5  " + minus(7, 2)); // 7 - 2
+		System.out.println("minus -5  " + minus(2, 7)); // 2 - 7
+		System.out.println("times 12  " + times(3, 4)); // 3 * 4
+		System.out.println("plus 16  " + plus(2, times(4, 2))); // 2 + 4 * 2
+		System.out.println("pow 125  " + pow(5, 3)); // 5^3
+		System.out.println("pow 243  " + pow(3, 5)); // 3^5
+		System.out.println("div 4  " + div(12, 3)); // 12 / 3
+		System.out.println("div 1  " + div(5, 5)); // 5 / 5
+		System.out.println("div 25/7  " + div(25, 7)); // 25 / 7
+		System.out.println("sqrt  6  " + sqrt(36));
+		System.out.println("sqrt " + sqrt(263169));
+		System.out.println("sqrt " + sqrt(76123));
+	}
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+
+		for (int i = 0; i < x2; i++)
+			x1++;
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+
+		for (int i = 0; i < x2; i++)
+			x1--;
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-        // Replace the following statement with your code
-		return 0;
+
+		int result = 0;
+		while (x2 > 0) {
+			result = plus(result, x1);
+			x2--;
+
+		}
+
+		return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+
+		int result = 1;
+		while (n > 0) {
+			result = times(x, result);
+			n--;
+		}
+
+		return result;
 	}
 
-	// Returns the integer part of x1 / x2 
+	// Returns the integer part of x1 / x2
 	public static int div(int x1, int x2) {
-        // Replace the following statement with your code
-		return 0;
+
+		int result = 0;
+		while ((times(x2, result) < x1)) {
+			result++;
+
+		}
+		if (times(x2, result) == x1) {
+			return result;
+		} else {
+			return minus(result, 1);
+		}
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-        // Replace the following statement with your code
-		return 0;
-	}	
+		int result = 0;
+		if ((times(div(x1, x2), x2) == x2)) {
+			return 0;
+		} else {
+			result = minus(x2, times(div(x1, x2), x2));
+		}
+		return result;
+	}
 
-	// Returns the integer part of sqrt(x) 
+	// Returns the integer part of sqrt(x)
 	public static int sqrt(int x) {
-        // Replace the following statement with your code
-		return 0;
-	}	  	  
+		int result = 0;
+		while (times(result, result) < x) {
+			System.out.print(result + " ");
+			result++;
+		}
+		if (times(result, result) == x) {
+			return result;
+		} else
+			return (minus(result, 1));
+
+	}
 }
